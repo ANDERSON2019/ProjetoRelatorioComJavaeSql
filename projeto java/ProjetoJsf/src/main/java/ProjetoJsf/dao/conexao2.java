67599f850550e5627d57312530b7b489084cae53
Gaby <Gaby@Seth-PC.Home>
2017-04-30 14:54:28 -0300

    anderson rocha de sousa

---
+++ projeto java/ProjetoJsf/src/main/java/ProjetoJsf/dao/conexao2.java
@@ -0,0 +1,19 @@
+package ProjetoJsf.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class fabricaDeConexao {
+	
+	private static final String URL = "jdbc:sqlserver://xxxxxx:xxxxxx;databaseName=xxxxxx";
+	private static final String Senha = "xxxxxx";
+	private static final String Login = "xxxxxxx";
+
+
+	public static Connection Conectar() throws SQLException {
+		DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+		Connection connection = DriverManager.getConnection(URL, Login, Senha);
+		return connection;
+	}
+}
