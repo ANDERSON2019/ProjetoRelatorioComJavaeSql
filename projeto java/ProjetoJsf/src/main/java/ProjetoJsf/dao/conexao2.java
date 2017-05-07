edd3f4fb77f28aa8dd631506028cd3ef636b9ae1
Gaby <Gaby@Seth-PC.Home>
2017-05-07 17:58:38 -0300

    xzx

--- projeto java/ProjetoJsf/src/main/java/ProjetoJsf/dao/conexao2.java
+++ projeto java/ProjetoJsf/src/main/java/ProjetoJsf/dao/conexao2.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class fabricaDeConexao {
 	
-	// teste 2 tetette
+	// teste 2 tetette sasa
 	
 	private static final String URL = "jdbc:sqlserver://xxxxxx:xxxxxx;databaseName=xxxxxx";
 	private static final String Senha = "xxxxxx";
